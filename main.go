f534cdd700ce2d67a8620f91cc86d70893f6af34
Yaskur 🇲 <dyas@yaskur.com>
2022-02-08 03:20:20 +0700

    Handle input test

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
 
+type Variable struct {
+	galaxyUnit   map[string]string
+	galaxyCredit map[string]float32
+}
+
+func translator(input string) string {
+	var output string
+
+	r, _ := regexp.Compile(`([.a-z]+) is ([.A-Z]+)$`)
+
+	// Using FindStringSubmatch you are able to access the
+	// individual capturing groups
+	for index, match := range r.FindStringSubmatch(input) {
+		fmt.Printf("[%d] %s\n", index, match)
+	}
+
+	return output
+}
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, _, err := reader.ReadLine()
+		str := string(text)
+		if str == "" || err != nil {
+			break
+		}
+		fmt.Println(translator(str))
+		//fmt.Println(str)
+	}
 }
