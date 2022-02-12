b584fc6ca2c73cf0bad4e439947b26b7cb68a7a6
Yaskur 🇲 <dyas@yaskur.com>
2022-02-12 20:24:57 +0700

    add more test case

--- translator_test.go
+++ translator_test.go
@@ -21,7 +21,7 @@ func TestExecute(t *testing.T) {
 		"how much wood could a woodchuck chuck if a woodchuck could chuck wood ?",
 	}
 
-	want := []string{
+	wants := []string{
 		"pish tegj glob glob is 42",
 		"glob prok Silver is 68 Credits",
 		"glob prok Gold is 57800 Credits",
@@ -29,23 +29,106 @@ func TestExecute(t *testing.T) {
 		"I have no idea what you are talking about",
 	}
 
+	doTest(input, wants, t)
+}
+
+//Unit test custom
+func TestExecute2(t *testing.T) {
+	input := []string{
+		"prek is I",
+		"vick is V",
+		"pish is X",
+		"tegj is L",
+		"anu is C",
+		"kae is D",
+		"lho is M",
+		"prek prek Silver is 34 Credits",
+		"prek vick Gold is 57800 Credits",
+		"pish pish Iron is 3910 Credits",
+		"kae anu Platinum is 30000 Credits",
+		"how much is pish tegj prek prek ?",
+		"how much is anu kae pish vick ?",
+		"how much is anu kae lho vick ?",
+		"how many Credits is prek vick Silver ?",
+		"how many Credits is prek vick Gold ?",
+		"how many Credits is prek vick Iron ?",
+		"how many Credits is prek vick Platinum ?",
+		"how many Credits is prek vick vick vick vick Platinum ?",
+		"how much nganu kae?",
+	}
+
+	wants := []string{
+		"pish tegj prek prek is 42",
+		"anu kae pish vick is 415",
+		"anu kae lho vick is 1405",
+		"prek vick Silver is 68 Credits",
+		"prek vick Gold is 57800 Credits",
+		"prek vick Iron is 782 Credits",
+		"prek vick Platinum is 200 Credits",
+		"Your units are not valid",
+		"I have no idea what you are talking about",
+	}
+
+	doTest(input, wants, t)
+}
+
+//Unit test custom
+func TestExecute3(t *testing.T) {
+	input := []string{
+		"poli is I",
+		"tali is V",
+		"deli is X",
+		"tegj is L",
+		"seli is C",
+		"kuli is D",
+		"peti is M",
+		"poli poli Uranium is 68 Credits",
+		"poli tali Vibranium is 128000 Credits",
+		"deli deli Plutonium is 87000 Credits",
+		"kuli seli Thorium is 15000 Credits",
+		"how much is deli tegj poli poli ?",
+		"how much is seli kuli deli tali ?",
+		"how much is seli kuli peti tali ?",
+		"how many Credits is poli tali Uranium ?",
+		"how many Credits is poli tali Vibranium ?",
+		"how many Credits is poli tali Plutonium ?",
+		"how many Credits is poli tali Thorium ?",
+		"how many Credits is poli poli poli poli tali Thorium ?",
+		"how much ngseli kuli?",
+	}
+
+	wants := []string{
+		"deli tegj poli poli is 42",
+		"seli kuli deli tali is 415",
+		"seli kuli peti tali is 1405",
+		"poli tali Uranium is 136 Credits",
+		"poli tali Vibranium is 128000 Credits",
+		"poli tali Plutonium is 17400 Credits",
+		"poli tali Thorium is 100 Credits",
+		"Your units are not valid",
+		"I have no idea what you are talking about",
+	}
+	doTest(input, wants, t)
+}
+
+func doTest(input []string, wants []string, t *testing.T) {
 	//Init Query Struct
 	var s System
 	s.Init()
 
-	//Execute Query line by line
+	//Translate input to action command
 	for _, line := range input {
 		s.Translate(line)
 	}
-
-	//t.Errorf(`%v %v`, s.galaxyCredit, s.galaxyUnit)
 	//Get output
 	output := s.GetOutput()
 
 	//Match the output with what we want
-	for i := range want {
-		if output[i] != want[i] {
-			t.Errorf(`False output = "%s", want "%s"`, output[i], want[i])
+	for i := range wants {
+		if output[i] != wants[i] {
+			t.Errorf(`False output = "%s", want "%s"`, output[i], wants[i])
 		}
 	}
 }
+
+//
